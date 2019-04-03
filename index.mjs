868d16d05f39f68c8e87c53054eda729964e02ed
Anwesh43 <anweshthecool0@gmail.com>
2019-04-04 00:16:43 +0530

    adding delay post each animation

--- index.mjs
+++ index.mjs
@@ -1,2 +1,12 @@
-import {createBlocks, startAnimation} from './BlockCreator'
-startAnimation(createBlocks())
+import {createBlocks} from './BlockCreator.mjs'
+var i = 0
+const blocks = createBlocks()
+const startAnimForBlock = () => {
+    var n = i
+    blocks[n].startAnimation()
+    i++
+    if (i < blocks.length) {
+        setTimeout(startAnimForBlock, 40)
+    }
+}
+startAnimForBlock()
